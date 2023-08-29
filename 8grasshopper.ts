23a1bb46d5768985db1fdfb7de336c5644d78f3a
Jackie Dai <jackydai12.21@gmail.com>
2023-08-29 14:10:38 -0400

    daily code

---
+++ 8grasshopper.ts
@@ -0,0 +1,11 @@
+
+/*This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+*/
+
+
+
+function checkForFactor (base: number, factor: number) : boolean {
+    return base % factor == 0
+}
