be41004d6dfefd398a3b89602c5c38a5d68c51a5
Jackie Dai <jackydai12.21@gmail.com>
2023-08-30 14:35:03 -0400

    codewars

---
+++ 6take-10-minute-walk.js
@@ -0,0 +1,23 @@
+/**
+ * @param {array}
+ * @return {boolean}
+ * example 
+ * Pseudocode
+ */
+
+function isValidWalk(walk) {
+    var dx = 0
+    var dy = 0
+    var dt = walk.length
+    
+    for (var i = 0; i < walk.length; i++) {
+      switch (walk[i]) {
+        case 'n': dy--; break
+        case 's': dy++; break
+        case 'w': dx--; break
+        case 'e': dx++; break
+      }
+    }
+    
+    return dt === 10 && dx === 0 && dy === 0
+  }
