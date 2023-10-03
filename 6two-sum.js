ecb5e0263c88fcfd83ce66404a163266efe3287d
Jackie Dai <jackydai12.21@gmail.com>
2023-10-03 10:28:25 -0400

    code

---
+++ 6two-sum.js
@@ -0,0 +1,21 @@
+function twoSum(nums, target){
+    let seen = new Map()
+    for (let i = 0; i<nums.length; i++){
+        let value = nums[i], diff = target - nums[i]
+        if (seen.has(diff))
+            return [seen.has(diff), i]
+        seen.set(value, i)
+    }
+}
+
+[2, 3, 4, 6] target = 10
+
+hashmap = {2: 0,
+           3: 1,
+           4: 2,
+           }
+
+value = 6
+diff = 4
+
+return [2, 3]
